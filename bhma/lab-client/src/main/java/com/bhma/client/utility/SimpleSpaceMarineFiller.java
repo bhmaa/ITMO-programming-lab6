171144969f57e54b2dcdd7b1b8179544c13794c0
Daria Starikova <96648193+bhmaa@users.noreply.github.com>
2022-03-15 15:36:25 +0300

    Update SimpleSpaceMarineFiller.java

--- bhma/lab-client/src/main/java/com/bhma/client/utility/SimpleSpaceMarineFiller.java
+++ bhma/lab-client/src/main/java/com/bhma/client/utility/SimpleSpaceMarineFiller.java
@@ -1,54 +1,46 @@
 package com.bhma.client.utility;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import com.bhma.client.exceptions.IllegalValueException;
 import com.bhma.client.exceptions.ScriptException;
 
 public class SimpleSpaceMarineFiller<T> {
     private final OutputManager outputManager;
-    private final SpaceMarineReader reader;
     private final InputManager inputManager;
 
     public SimpleSpaceMarineFiller(InputManager inputManager, OutputManager outputManager, SpaceMarineReader reader) {
         this.inputManager = inputManager;
         this.outputManager = outputManager;
-        this.reader = reader;
     }
     /**
      * selection correct values of some SpaceMarine field
      * @param message message that will be written if input manager is not reading from a file now
-     * @param readingMethod name of SpaceMarineReader's method that will be invoked
+     * @param reader SpaceMarineReader's method that will be invoked or other lambda expression
      * @return correct value of field
      * @throws ScriptException if the value was incorrect and input manager read from a file
      */
 
-    public T fill(String message, String readingMethod) throws ScriptException {
+    public T fill(String message, Reader<T> reader) throws ScriptException {
         T returns;
-        Method read;
         while (true) {
             try {
-                read = reader.getClass().getMethod(readingMethod);
                 outputManager.print(message + ": ");
-                returns = (T) read.invoke(reader);
+                returns = reader.read();
                 break;
-            } catch (InvocationTargetException e) {
-                if (e.getCause() instanceof NumberFormatException) {
-                    outputManager.println("Value must be a number");
-                } else {
-                    if (e.getCause() instanceof IllegalArgumentException) {
-                        outputManager.println("Chose anything from list");
-                    }
-                    if (e.getCause() instanceof IllegalValueException) {
-                        outputManager.println(e.getCause().getMessage());
-                    }
-                }
+            } catch (NumberFormatException e) {
+                outputManager.println("Value must be a number");
                 if (inputManager.getScriptMode()) {
                     throw new ScriptException();
                 }
-            } catch (NoSuchMethodException | SecurityException | IllegalAccessException e) {
-                outputManager.println("Something went wrong...");
+            } catch (IllegalArgumentException e) {
+                outputManager.println("Chose anything from list");
+                if (inputManager.getScriptMode()) {
+                    throw new ScriptException();
+                }
+            } catch (IllegalValueException e) {
+                outputManager.println(e.getMessage());
+                if (inputManager.getScriptMode()) {
+                    throw new ScriptException();
+                }
             }
         }
         return returns;
