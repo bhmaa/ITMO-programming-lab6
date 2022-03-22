9a6292683050e1382dd62fb331778051cbfd9d74
bhmaa <ddz.sm@yandex.ru>
2022-03-22 10:02:32 +0300

    javadoc

--- bhma/lab-client/src/main/java/com/bhma/client/exceptions/InvalidInputException.java
+++ bhma/lab-client/src/main/java/com/bhma/client/exceptions/InvalidInputException.java
@@ -1,5 +1,8 @@
 package com.bhma.client.exceptions;
 
+/**
+ * throws if entered value was ctrl+d
+ */
 public class InvalidInputException extends Exception {
     public String getMessage() {
         return "Invalid input. Work with the collection will be finished";
